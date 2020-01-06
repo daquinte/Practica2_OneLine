755a24692b45fc34c5cd8be7119788b65bdd88d1
adralcan <adrian97ad@gmail.com>
2020-01-06 17:06:35 +0100

    Mejora del timer

--- Assets/Scripts/Juego/Menu/CountDown.cs
+++ Assets/Scripts/Juego/Menu/CountDown.cs
@@ -17,55 +17,43 @@ public class CountDown : MonoBehaviour
 
     public bool stop = true;
 
+    public Text text;
 
-    //timeLeft -= TimeManager.instance.CheckDate(); // Esto en el init
-    //timeLeft -= Time.deltaTime; // Esto en el Update
+    private void OnEnable() {
+        StartTimer();
+    }
+
+    public void StartTimer() {
+        stop = false;
+        TimeManager.instance.SaveDate();
+        timeLeft -= TimeManager.instance.CheckDate();
+        Update();
+        StartCoroutine(updateCoroutine());
+    }
+
+    public void StopTimer() {
+        stop = true;
+    }
+
+    void Update() {
+        if (stop) return;
+        timeLeft -= Time.deltaTime;
+        Debug.Log("Tiempo: " + timeLeft);
+        minutes = Mathf.Floor(timeLeft / 60);
+        seconds = Mathf.Round(timeLeft % 60);
+        if (minutes < 0) {
+            stop = true;
+            minutes = 0;
+            seconds = 0;
+            canvasJuego.ChallengeFallido();
+        }
+    }
 
     void ResetClock(float time)
     {
         TimeManager.instance.SaveDate();
         timeLeft = time;
         timeLeft -= TimeManager.instance.CheckDate();
-    }
-
-
-    public Text text;
-
-    private void OnEnable()
-    {
-        StartTimer(timeLeft);
-    }
-
-    public void StartTimer(float from)
-    {
-        stop = false;
-        timeLeft = from;
-        Update();
-        StartCoroutine(updateCoroutine());
-    }
-
-    public void StopTimer()
-    {
-        stop = true;
-    }
-
-    void Update()
-    {
-        if (stop) return;
-        timeLeft -= Time.deltaTime;
-
-        minutes = Mathf.Floor(timeLeft / 60);
-        seconds = timeLeft % 60;
-        if (seconds > 59) seconds = 59;
-        if (minutes < 0)
-        {
-            stop = true;
-            minutes = 0;
-            seconds = 0;
-
-            canvasJuego.ChallengeFallido();
-        }
-        //        fraction = (timeLeft * 100) % 100;
     }
 
     private IEnumerator updateCoroutine()
